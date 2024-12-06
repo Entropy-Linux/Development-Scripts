edc8f4fb73c4e3bf830478ae005a687626ff56bc
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2024-12-06 19:02:38 +0100

    Create 0.sh

---
+++ mergstall/0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# STAGE 0
+
+# Define variables
+LIVE_ISO_ROOT="/"
+TARGET_ROOT="/mnt/target/"     
+SPAWN="$target_mount_point/szmelc"   
+     
+EXCLUDE_CONF="excluded.conf"            
+INCLUDE_CONF="included.conf"
+USE_WHITELIST=true                
+       
